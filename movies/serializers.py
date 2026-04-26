df74289ade7e54db0981caf0cba0e5df8394eb44
favio <favio.solorzano@tecsup.edu.pe>
2026-04-26 09:58:12 -0500

    Primer commit : creando Los Generos de las peliculas

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
 
 class MovieSerializer(serializers.ModelSerializer):
+    
+    genres = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Genre.objects.all()
+    )
+
     class Meta:
         model = Movie
         fields = '__all__'
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Movie
-from .serializers import MovieSerializer
+from .models import Movie, Genre  # CAMBIO
+from .serializers import MovieSerializer, GenreSerializer  # CAMBIO
 
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+
+class GenreViewSet(ModelViewSet):  # NUEVO
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
